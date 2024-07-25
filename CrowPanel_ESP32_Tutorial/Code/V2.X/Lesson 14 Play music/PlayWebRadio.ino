baec48761b6bdd48cd0ba4dd528779a1db34dce6
Ludovic BOUÉ <lboue@users.noreply.github.com>
2024-07-25 14:16:11 +0200

    Update PlayWebRadio.ino

--- CrowPanel_ESP32_Tutorial/Code/V2.X/Lesson 14 Play music/PlayWebRadio.ino
+++ CrowPanel_ESP32_Tutorial/Code/V2.X/Lesson 14 Play music/PlayWebRadio.ino
@@ -1,56 +1,102 @@
-#include <Wire.h>
-#include <SPI.h>
-#include <SD.h>
-#include <FS.h>
-#include <Audio.h> // https://github.com/schreibfaul1/ESP32-audioI2S
-Audio audio;
+//**********************************************************************************************************
+//*    audioI2S-- I2S audiodecoder for ESP32                                                              *
+//**********************************************************************************************************
+//
+// first release on 11/2018
+// Version 3  , Jul.02/2020
+//
+//
+// THE SOFTWARE IS PROVIDED "AS IS" FOR PRIVATE USE ONLY, IT IS NOT FOR COMMERCIAL USE IN WHOLE OR PART OR CONCEPT.
+// FOR PERSONAL USE IT IS SUPPLIED WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHOR
+// OR COPYRIGHT HOLDER BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
+//
 
+#include "Arduino.h"
+#include "WiFiMulti.h"
+#include "Audio.h"
+#include "SPI.h"
+#include "SD.h"
+#include "FS.h"
+
+// Digital I/O used
 #define I2S_DOUT      17
 #define I2S_BCLK      42
 #define I2S_LRC       18
 
 //7.0
-#define SD_MOSI 11
-#define SD_MISO 13
-#define SD_SCK 12
-#define SD_CS 10
+#define SD_MOSI       11
+#define SD_MISO       13
+#define SD_SCK        12
+#define SD_CS         10
 
-const char *ssid = "********"; //Change to your SSID
-const char *password = "********"; //Change to your password
+Audio audio;
+WiFiMulti wifiMulti;
+String ssid =     "********"; //Change to your SSID
+String password = "IOT4449TESTING";
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200); /*Initialize the serial port*/
-  Serial.println("Audio example");
+    pinMode(SD_CS, OUTPUT);
+    digitalWrite(SD_CS, HIGH);
+    SPI.begin(SD_SCK, SD_MISO, SD_MOSI);
+    SPI.setFrequency(1000000);
+    Serial.begin(115200);
+    SD.begin(SD_CS);
+    WiFi.mode(WIFI_STA);
+    wifiMulti.addAP(ssid.c_str(), password.c_str());
+    wifiMulti.run();
+    if(WiFi.status() != WL_CONNECTED){
+        WiFi.disconnect(true);
+        wifiMulti.run();
+    }
+    audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
+    audio.setVolume(12); // 0...21
 
-  pinMode(SD_CS, OUTPUT);
-  digitalWrite(SD_CS, HIGH);
-  SPI.begin(SD_SCK, SD_MISO, SD_MOSI);
-  SPI.setFrequency(1000000);
-  SD.begin(SD_CS);
-
-  WiFi.begin(ssid, password);
-  WiFi.setAutoReconnect(true);//Set up automatic reconnection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(100);
-    Serial.println("connecting to WiFi");
-  }
-  Serial.println("WiFi is connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
-  audio.setVolume(21); // 0...21
-
-  //  *** radio streams ***
-  audio.connecttohost("http://stream.radioparadise.com/flac");
-
-  //*** local files ***
-  //audio.connecttoFS(SD, "/123.mp3");
+//    audio.connecttoFS(SD, "test.wav");
+//    audio.connecttohost("http://www.wdr.de/wdrlive/media/einslive.m3u");
+//    audio.connecttohost("http://somafm.com/wma128/missioncontrol.asx"); //  asx
+//    audio.connecttohost("http://mp3.ffh.de/radioffh/hqlivestream.aac"); //  128k aac
+      audio.connecttohost("http://mp3.ffh.de/radioffh/hqlivestream.mp3"); //  128k mp3
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  audio.loop();
-  //audio.stopSong();//close
+void loop()
+{
+    audio.loop();
+    if(Serial.available()){ // put streamURL in serial monitor
+        audio.stopSong();
+        String r=Serial.readString(); r.trim();
+        if(r.length()>5) audio.connecttohost(r.c_str());
+        log_i("free heap=%i", ESP.getFreeHeap());
+    }
 }
+
+// optional
+void audio_info(const char *info){
+    Serial.print("info        "); Serial.println(info);
+}
+void audio_id3data(const char *info){  //id3 metadata
+    Serial.print("id3data     ");Serial.println(info);
+}
+void audio_eof_mp3(const char *info){  //end of file
+    Serial.print("eof_mp3     ");Serial.println(info);
+}
+void audio_showstation(const char *info){
+    Serial.print("station     ");Serial.println(info);
+}
+void audio_showstreamtitle(const char *info){
+    Serial.print("streamtitle ");Serial.println(info);
+}
+void audio_bitrate(const char *info){
+    Serial.print("bitrate     ");Serial.println(info);
+}
+void audio_commercial(const char *info){  //duration in sec
+    Serial.print("commercial  ");Serial.println(info);
+}
+void audio_icyurl(const char *info){  //homepage
+    Serial.print("icyurl      ");Serial.println(info);
+}
+void audio_lasthost(const char *info){  //stream URL played
+    Serial.print("lasthost    ");Serial.println(info);
+}
+
